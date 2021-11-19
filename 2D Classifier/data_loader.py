006b1f5c42a6c309eb8de044ddc5e94cee1ce729
matthew-tucsok <matt.tucsok@gmail.com>
2021-11-18 17:31:21 -0800

    Completed dataset loading and vectorization

---
+++ 2D Classifier/data_loader.py
@@ -0,0 +1,39 @@
+import glob
+
+from PIL import Image
+import numpy as np
+
+
+class DataLoader:
+    def __init__(self, path):
+        self.source = path
+
+        self.samples = []
+
+        self.load_data()
+
+    def load_data(self):
+        files = glob.glob(self.source + '/*.png')
+        for file in files:
+            img = Image.open(file)
+
+            splits = file.split('\\')
+            class_and_index = splits[-1]
+            label_name, _ = class_and_index.split('_', 2)
+            label = None
+            if label_name == 'f':
+                label = 1
+            elif label_name == 'nf':
+                label = 0
+            else:
+                raise SyntaxError('Invalid class label detected')
+            self.samples.append([img, label])
+
+    def vectorize_data(self):
+        index = 0
+        for sample in self.samples:
+            img_array = np.array(sample[0])
+            img_vector = img_array.ravel()
+            self.samples[index][0] = img_vector
+            index += 1
+
--- 2D Classifier/main.py
+++ 2D Classifier/main.py
@@ -1,6 +1,17 @@
+import os
+
+from data_loader import DataLoader
+
+
 def main():
-    pass
+    cwd = os.getcwd()
+    os.chdir(cwd)
+    dataset = DataLoader(cwd + '/Greyscale Dataset')
+    dataset.vectorize_data()
+    print(len(dataset.samples[0][0]))
+    print(dataset.samples[0][1])
 
 
 if __name__ == '__main__':
     main()
+
