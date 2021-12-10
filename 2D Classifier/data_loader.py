ee0588af36e8a1715b839b9abc2318ee88fa5c0e
matthew-tucsok <matt.tucsok@gmail.com>
2021-12-09 16:42:22 -0800

    Refactored method and comments
    - added comments to all parts of code
    - Finalized which lines are commented out or not for final functionality
    - finalized which features we are using

--- 2D Classifier/data_loader.py
+++ 2D Classifier/data_loader.py
@@ -5,49 +5,101 @@ import numpy as np
 
 
 class DataLoader:
+    """
+    This class manages how images are loaded in, preprocessed, and assembled into an input format compatible with our
+    2D classifier model. Note that due to how we create our training and test sets, both sets get preprocessed.
+    """
     def __init__(self, path):
+        """
+        Initializing the data loader to find the dataset images.
+        :param path: Path to the root of the program. The root should contain the executing script, and a folder
+        named "Greyscale Dataset" which directly has the 4000 images in the dataset.
+        """
         self.source = path
-        self.samples = []
-        self.calculated_features = []
-        self.load_data()
+        self.samples = []  # List to hold the dataset samples
+        self.calculated_features = []  # List to hold the calculated features that will be used as input to the model
+        self.load_data()  # Begin data loading method
 
     def load_data(self):
-        files = glob.glob(self.source + '/*.png')
+        files = glob.glob(self.source + '/*.png')  # This will put all the .png filenames into a list
 
-        for file in files:
-            img = Image.open(file).convert('L')
+        # index = 0
+        for file in files:  # Iterate over all files in the dataset
+            img = Image.open(file).convert('L')  # Open the greyscale images, specifying 'L' to ensure correct loading
+
+            """
+            The following 3 line were for visualization purposes only in the report. They select a specific image to
+            show how the original greyscale image looks before pre-processing. 
+            """
+            # grey = Image.open(file).convert('L')
+            # if index % 3334 == 0 and index > 0:
+            #     img.show()
+
+            """
+            # img_h is a histogram with 256 bins with each bin n counting how many times a pixel with a value of n
+            appears in the image. 
+            """
             img_h = np.array(img.histogram())
-            var = np.var(img_h)
+            var = np.var(img_h)  # Calculating the variance of the histogram. This is one of our final features
+
+            """
+            img.filter(ImageFilter.FIND_EDGES) generates an edge-detected image with the same dimensions as the original
+            greyscale image. 
+            """
             img = img.filter(ImageFilter.FIND_EDGES)
-            img_array = np.array(img)
-            mean = np.sum(img_array)/(64*64)
-            binary_img = np.where(img_array > mean, 1, 0)
-            edge_pixel_sum = np.sum(binary_img)
-            # pil_binary_img = Image.fromarray(binary_img*255)
-            # pil_binary_img.show()
 
-            splits = file.split('\\')
+            # The following 2 lines were for showing the edge-detected image for the report
+            # if index % 3334 == 0 and index > 0:
+            #     img.show()
 
-            class_and_index = splits[-1]
-            label_name, _ = class_and_index.split('_', 2)
+            img_array = np.array(img)  # Ensuring PIL image is now a numpy array
+
+            # Calculating the mean of the edge-detected image
+            mean = np.sum(img_array)/(img_array.shape[0]*img_array.shape[1])
+
+            binary_img = np.where(img_array > mean, 1, 0)  # Thresholding edge-detected image based on mean pixel value
+            edge_pixel_sum = np.sum(binary_img)  # Summing all of the pixel values in the binary image. 2nd final feature
+
+            # Following 4 lines used to display the binary image for visualization in the report.
+            # if index % 3334 == 0 and index > 0:
+            #     print(index)
+            #     pil_binary_img = Image.fromarray(binary_img*255)
+            #     pil_binary_img.show()
+
+            splits = file.split('\\')  # Parsing the file name for label extraction
+
+            class_and_index = splits[-1]  # Getting the last split which contain the name of the image with filetype
+            label_name, _ = class_and_index.split('_', 2)  # Getting only the 'f' or 'nf' part of the image name
             label = None
             if label_name == 'f':
-                label = 1
+                label = 1  # Flat images are class 1
             elif label_name == 'nf':
-                label = 0
+                label = 0  # Non-flat images are class 0
             else:
-                raise SyntaxError('Invalid class label detected')
-            self.calculated_features.append(np.array([var, edge_pixel_sum]))
-            # self.samples.append([img, label])
+                raise SyntaxError('Invalid class label detected')  # Just a check to see if data was loaded correctly
+            self.calculated_features.append(np.array([var, edge_pixel_sum]))  # Appending the features to the feature list
+            # self.samples.append([img, label])  # (Deprecated)
             self.samples.append([binary_img, label])
+            # self.samples.append([grey, label])
+            # index += 1
 
+        """
+        The following block of code is where the final feature vector is generated as input to the model. This section
+        contains commented code and visibly redundant lines to keep above code consistent for all iterations of input
+        while still maintaining functionality. For example, in the final feature vector we do not include image pixels,
+        so vectorization of the image is no longer required and therefore replaced with an empty array to remove these
+        features
+        """
         index = 0
-        for sample in self.samples:
-            img_array = np.array(sample[0])
-            img_vector = img_array.ravel()
-            img_vector = np.array([])
+        for sample in self.samples:  # Iterating over all samples in the dataset
+            img_array = np.array(sample[0])  # (Deprecated) Extracting the previous image associated with a sample
+            img_vector = img_array.ravel()  # (Deprecated) vectorizing the 64 x 64 image to a 4096 x 1 vector
+            img_vector = np.array([])  # Removing the image vector as it was not used in the final iteration of the model
+
+            # (Deprecated) Used to remove calculated features to test only the performance of vectorized image inputs
             # self.calculated_features[index] = np.array([])
+
+            # The line below will keep whatever features still exist after the decisions made above
             sample_vector = np.hstack((img_vector, self.calculated_features[index]))
             self.samples[index][0] = sample_vector
             index += 1
-
--- 2D Classifier/main.py
+++ 2D Classifier/main.py
@@ -1,5 +1,6 @@
 import os
 import random
+import time
 
 from data_loader import DataLoader
 from model import Model
@@ -8,22 +9,36 @@ import numpy as np
 
 
 def main():
-    cwd = os.getcwd()
-    os.chdir(cwd)
+    """
+    The main executing function. This function is responsible for finding the path to our dataset, creating an instance
+    of our dataset, creating an instance of our model, and running the cross-validation of our model.
+    """
+    cwd = os.getcwd()  # Getting the root directory of the folder that executed the main function
+    os.chdir(cwd)  # Ensuring this is also set to the current directory
 
-    runs = 10
-    accuracies = []
-    for run in range(runs):
-        print('Run', run+1)
-        dataset = DataLoader(cwd + '/Greyscale Dataset')
-        train_test_split = 0.75
+    """
+    # "runs" controls how many times the model is trained and tested. This should be performed multiple times for 
+    evaluating model accuracy since the training and testing sets are randomly generated with a roughly 50/50 split of
+    non-flat and flat images in each set. This ensemble cross-validation method of evaluation uses the law of large numbers to find the
+    true general accuracy of the final model. 
+    """
+    runs = 100  # Large enough that average accuracy will have a decent confidence based on variance of model
+    accuracies = []  # List to store test accuracies for cross validation
+    start = time.time()  # Start time for timing the total execution of the multiple runs of the program
+    for run in range(runs):  # Each run is an independent evaluation of performance
+        print('Run', run + 1)
+        dataset = DataLoader(cwd + '/Greyscale Dataset')  # Creating an instance of the dataset
+        random.shuffle(dataset.samples)  # Randomizing the order of the dataset to ensure order invariance
+        train_test_split = 0.75  # We selected a 75% training 25% testing for the model training and evaluation
+        split_index = round(len(dataset.samples) * train_test_split)  # Determining where to split the dataset
+        training_set = dataset.samples[:split_index]  # Extracting the training set from the shuffled dataset
+        test_set = dataset.samples[split_index:]  # Extracting the testing set from the shuffled dataset
 
-        random.shuffle(dataset.samples)
-        train_test_split = 0.75
-        split_index = round(len(dataset.samples) * train_test_split)
-        training_set = dataset.samples[:split_index]
-        test_set = dataset.samples[split_index:]
-
+        """
+        The following block of code is for visualizing how many images are flat and how many are non-flat for a training
+        set. This was to see if the model is biased towards the class with more examples. This did not seem to be too
+        much of a problem in testing.
+        """
         flat_label_count = 0
         not_flat_label_count = 0
         for sample in training_set:
@@ -31,11 +46,13 @@ def main():
                 flat_label_count += 1
             else:
                 not_flat_label_count += 1
-
         print(flat_label_count, 'flat images and', not_flat_label_count, 'non-flat images in training set.')
 
-        classifier = Model(training_set, 0.001, 1e-7)
+        classifier = Model(training_set)  # Creating an instance of the classifier. This initiates model training.
 
+        """
+        Code block below checks the number of flat and non-flat images in the test set.
+        """
         flat_label_count = 0
         not_flat_label_count = 0
         for sample in test_set:
@@ -43,23 +60,28 @@ def main():
                 flat_label_count += 1
             else:
                 not_flat_label_count += 1
-
         print(flat_label_count, 'flat images and', not_flat_label_count, 'non-flat images in test set.')
 
-        confusion_matrix = classifier.accuracy(test_set)
+        confusion_matrix = classifier.accuracy(training_set)  # Getting the confusion matrix of the training_set
+        accuracy = 100 * np.trace(confusion_matrix) / np.sum(confusion_matrix)  # Calculating accuracy of training set
         print(confusion_matrix)
-        accuracy = 100 * np.trace(confusion_matrix) / np.sum(confusion_matrix)
-        accuracies.append(accuracy)
-        print('Accuracy:', accuracy, '%')
+        print('Training Accuracy:', accuracy, '%')
 
-    accuracies = np.array(accuracies)
+        confusion_matrix = classifier.accuracy(test_set)  # Getting the confusion matrix of the test set
+        print(confusion_matrix)
+        accuracy = 100 * np.trace(confusion_matrix) / np.sum(confusion_matrix)  # Calculating accuracy of test set
+        accuracies.append(accuracy)  # Appending only the test accuracy to the list of accuracy for cross-validation
+        print('Testing Accuracy:', accuracy, '%')
+
+    accuracies = np.array(accuracies)  # Converting python list to numpy array for accuracies
     mean_accuracy = np.mean(accuracies)
-    var_accuracy = np.var(accuracies)
-    std_accuracy = np.sqrt(var_accuracy)
+    var_accuracy = np.var(accuracies)  # Variance of accuracies
+    std_accuracy = np.sqrt(var_accuracy)  # Standard deviation of accuracies
     print('Average of Accuracy:', mean_accuracy, '%')
     print('Variance of Accuracy:', var_accuracy)
     print('Standard Deviation of Accuracy:', std_accuracy)
+    print('Runtime for', runs, 'runs:', round(time.time() - start, 2), 'seconds')  # Complete runtime for the runs
 
 
-if __name__ == '__main__':
+if __name__ == '__main__':  # Python convention for ensuring recursive calling of main function does not occur
     main()
